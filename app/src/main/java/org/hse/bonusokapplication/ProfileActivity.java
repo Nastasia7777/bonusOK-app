7f017c623b2807cca4bd22ead76acf0fbddc66e2
Дарья Кузнецова <dakuznetsova_3@edu.hse.ru>
2021-05-13 22:15:26 +0500

    Добавлен идентификатор QR кода Изменен манифест Добавлены библиотеки в gradle В ProfileActivity происходит генерация QR

--- app/src/main/java/org/hse/bonusokapplication/ProfileActivity.java
+++ app/src/main/java/org/hse/bonusokapplication/ProfileActivity.java
@@ -1,11 +1,21 @@
 package org.hse.bonusokapplication;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
 public class ProfileActivity extends AppCompatActivity {
+
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +23,9 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
 
          View edit_profile_btn= findViewById(R.id.btn_edit_profile);
+
+        imageView = (ImageView)findViewById(R.id.qr);
+        createQr("1234567891112222");
 
         edit_profile_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,4 +37,17 @@ public class ProfileActivity extends AppCompatActivity {
          Intent intent = new Intent(this, EditingProfileActivity.class);
          startActivity(intent);
      }
+
+    private void createQr(String code){
+        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+        try{
+            BitMatrix bitMatrix = multiFormatWriter.encode(code, BarcodeFormat.QR_CODE, 500, 500);
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+            Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+            imageView.setImageBitmap(bitmap);
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 }
